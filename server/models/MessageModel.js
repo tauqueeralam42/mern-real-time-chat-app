d1bb8430d979d85a8f918f4aa0d9299ac8151e47
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-06-29 04:34:59 +0530

    message schema is created

---
+++ server/models/MessageModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const messageSchema = new mongoose.Schema({
+    text : {
+        type : String,
+        default : ""
+    },
+    imageUrl : {
+        type : String,
+        default : ""
+    },
+    videoUrl : {
+        type : String,
+        default : ""
+    },
+    seen : {
+        type : Boolean,
+        default : false
+    },
+    msgByUserId : {
+        type : mongoose.Schema.ObjectId,
+        required : true,
+        ref : 'User'
+    }
+},{
+    timestamps : true
+})
+
+const MessageModel = mongoose.model('Message',messageSchema);
+module.exports =  MessageModel;
