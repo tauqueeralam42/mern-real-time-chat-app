649ac2fed5acd629dcacc5262b9d39a2b8ec5b77
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-06-29 04:12:47 +0530

    server made

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+
+require('dotenv').config()
+
+const app = express()
+
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+
+
+const PORT = process.env.PORT || 8080
+
+app.get('/', (req,res) => {
+    res.json({
+        message : `Server is running on PORT no. ${PORT}`
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT no. ${PORT}`);
+})
