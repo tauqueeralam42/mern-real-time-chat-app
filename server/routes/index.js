b676b3529178ddf124013392d99b69d00cbabebf
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-06-29 05:20:03 +0530

    check emaail route is added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const registerUser = require('../controller/registerUser');
+const checkEmail = require('../controller/checkEmail');
 
 const router = express.Router();
 
 //create new user api
 router.post('/register',registerUser);
+
+//check user email exist or not
+router.post('/email',checkEmail)
 
 
 module.exports = router;
