b9664f8015cfde2c416a31c62e518ecade1f8ef3
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-06-29 05:50:41 +0530

    get user details route is added

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const registerUser = require('../controller/registerUser');
 const checkEmail = require('../controller/checkEmail');
 const checkPassword = require('../controller/checkPassword');
+const userDetails = require('../controller/userDetails');
 
 const router = express.Router();
 
@@ -13,6 +14,9 @@ router.post('/email',checkEmail);
 
 //check user password
 router.post('/password',checkPassword);
+
+//get login user details
+router.get('/user-details',userDetails);
 
 
 module.exports = router;
