b548c1643d9d4c051bdbe29a068b44fce3ceaf63
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-06-29 23:51:45 +0530

    frontend completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,16 @@ import './index.css';
 import App from './App';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/index';
-
+import { Provider } from 'react-redux'
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
+    <Provider store={store}>
       <RouterProvider router={router}>
         <App />
       </RouterProvider>
-    
+    </Provider>
   </React.StrictMode>
 );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        "primary" : "#0c283f",
+        "secondary" : "#01223d"
+
+      }
+    },
   },
   plugins: [],
 }
