8a2f6760d006f6efe93ea6931aa7bdd8f9c0e6f5
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-06-29 05:48:40 +0530

    get user details controller is added

---
+++ server/controller/userDetails.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken')
+const UserModel = require('../models/UserModel')
+
+
+async function userDetails(request,response){
+    try {
+        const token = request.cookies.token || ""
+
+        const user = await getUserDetailsFromToken(token)
+
+        return response.status(200).json({
+            message : "user details",
+            data : user
+        })
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true
+        })
+    }
+}
+
+const getUserDetailsFromToken = async(token)=>{
+    
+    if(!token){
+        return {
+            message : "session out",
+            logout : true,
+        }
+    }
+
+    try {
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const user = await UserModel.findById(decoded.id).select('-password');
+        return user;
+    } catch (error) {
+        console.error("Error verifying token:", error.message);
+        return null; 
+    }
+}
+
+
+
+module.exports = userDetails
